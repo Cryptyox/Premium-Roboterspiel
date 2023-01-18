2517b4c8ffee006673761dc7ea95ad9bac261116
Cryptyox <95845131+Cryptyox@users.noreply.github.com>
2023-01-18 20:37:07 +0100

    broke some stuff, tried ui

---
+++ Robot/PlayButton.gd
@@ -0,0 +1,17 @@
+extends TextureButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	
+
+func _button_pressed():
+	print("Hello world!")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
