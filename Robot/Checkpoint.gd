5463c975f7dd174dea73343e4a4ab69f69b5351d
Cryptyox <95845131+Cryptyox@users.noreply.github.com>
2023-01-22 18:38:07 +0100

    Checkpoint Implemented

---
+++ Robot/Checkpoint.gd
@@ -0,0 +1,24 @@
+extends Spatial
+
+var respawn_pos
+var is_current_checkpoint = false
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	respawn_pos = $Respawnpoint.get_global_transform().origin
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area_body_entered(player):
+	if player.has_method("set_checkpoint"):
+		if !is_current_checkpoint:
+			player.set_checkpoint(respawn_pos, self)
