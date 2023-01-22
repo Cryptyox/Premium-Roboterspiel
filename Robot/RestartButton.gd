b201f6298314620747996d558cf1273825cd6e26
Cryptyox <95845131+Cryptyox@users.noreply.github.com>
2023-01-22 18:52:46 +0100

    Die_RespawnButton

--- Robot/RestartButton.gd
+++ Robot/RestartButton.gd
@@ -11,7 +11,7 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_button_pressed():
-	global_transform.origin = respawn_point
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
