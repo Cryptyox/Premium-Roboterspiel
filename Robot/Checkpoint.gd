173eb4c54e9d60562bd20fd6337a56db53876230
lasiv <benni@anagami.de>
2023-01-23 10:31:19 +0100

    checkpoint works with new area on robot

--- Robot/Checkpoint.gd
+++ Robot/Checkpoint.gd
@@ -2,27 +2,21 @@ extends Spatial
 
 var respawn_pos
 var is_current_checkpoint = false
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	respawn_pos = $Respawnpoint.get_global_transform().origin
-	pass # Replace with function body.
+	
+func is_active():
+	return is_current_checkpoint
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_pos():
+	return respawn_pos
 
 func activate_checkpoint():
-	print("Checkpoint set1")
-	pass
+	print("Checkpoint set")
+	is_current_checkpoint = true
+	return respawn_pos
 
-func _on_Area_body_entered(player):
-	if player.has_method("set_checkpoint"):
-		if !is_current_checkpoint:
-			player.set_checkpoint(respawn_pos, self)
-			print("Checkpoint set2")
+func deactivate_checkpoint():
+	pass
