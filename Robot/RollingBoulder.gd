e696c941dc9d6dc63faf648a0d61683ec415dfd9
Hvnt0r <oskar.jaeger4321@web.de>
2023-01-26 16:02:08 +0100

    .
    boulder + collision

---
+++ Robot/RollingBoulder.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+
+const GRAVITY = 16
+#var boulder_velocity = 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
