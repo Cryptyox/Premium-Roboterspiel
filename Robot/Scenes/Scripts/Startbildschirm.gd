dd352bd88501aa96ffccf4adff6d949ec24f1816
lasiv <benni@anagami.de>
2023-01-27 09:29:18 +0100

    idk

--- Robot/Scenes/Scripts/Startbildschirm.gd
+++ Robot/Scenes/Scripts/Startbildschirm.gd
@@ -4,7 +4,7 @@ extends Control
 var master_bus = AudioServer.get_bus_index("Master")
 
 # when Volume slider is changed -> changes master_bus audio db value to given value
-func _on_HSlider_value_changed(value):
+func _on_VolumeSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(master_bus, value)
 
 	#if slider is at lowest: mute the bus entirely
@@ -13,4 +13,12 @@ func _on_HSlider_value_changed(value):
 	else:
 		AudioServer.set_bus_mute(master_bus,false)
 
+func _on_EffectSlider_value_changed(value):
+	pass
+	#AudioServer.set_bus_volume_db(master_bus, value)
 
+	#if slider is at lowest: mute the bus entirely
+	#if value ==-30:
+		#AudioServer.set_bus_mute(master_bus,true)
+	#else:
+		#AudioServer.set_bus_mute(master_bus,false)
