e108f354946a082a68e14cf43863cbb800b714f0
lasiv <benni@anagami.de>
2023-02-05 17:59:42 +0100

    made some more problems

--- Robot/Scenes/Scripts/HomeScreen.gd
+++ Robot/Scenes/Scripts/HomeScreen.gd
@@ -1,16 +1,15 @@
 extends Control
 
-
 func _on_ExitButton_pressed():
 	get_tree().quit()
 
 
 func _on_SettingsButton_pressed():
-	get_node("../SettingsPopup").show()
+	get_node("../SettingsScreen/SettingsPopup").show()
 
 
 func _on_PlayButton_pressed():
-	get_tree().change_scene("res://Scenes/1_TestWorld.tscn")
+	get_tree().change_scene("res://Scenes/UI_World.tscn")
 
 
 func _on_CheeseButton_pressed():
