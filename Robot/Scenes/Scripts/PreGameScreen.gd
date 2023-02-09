218ae6ae3ba46b5bd7f43ebe55aa2b7dbc3495b2
lasiv <benni@anagami.de>
2023-02-09 16:22:42 +0100

    lots of ui stuff

--- Robot/Scenes/Scripts/PreGameScreen.gd
+++ Robot/Scenes/Scripts/PreGameScreen.gd
@@ -5,13 +5,21 @@ signal closeWorld
 
 signal openHome
 
+
+signal showLevelText
+
+var text1 = "Disturbance in the Force you felt? It is not Megatron training backflips. Something terrible must have happened. Find out what it is you must!"
+var text2 = "Oh no, the monster is in outrage killing demons in hell hoping that they will drop a voodoo doll of the guide into the lava.\nGo save him!"
+var text3 = "The monsters name was leaked. JOHN has not been careful enough when entering the Frozen Computer Lab. But what does he want there? Go get them Tiger!"
+var text4 = "This is JOHNs lair. Now he can't go back to his own world anymore so he will have to bring harm to this one. He has built himself a fortress. Not the Flying Fortress of course."
+
 func _draw():
 	pass # move cam
 	
 func _on_SelectChapter_pressed(spawn):
-	var robot = get_node("../../ContainsWorld/Robot")
-	robot.translation = spawn
-	robot.set_spawn(spawn)
+	# do spawnpoint
+	var text = "."
+	emit_signal("showLevelText", text)
 
 
 func _on_HomeButton_pressed():
