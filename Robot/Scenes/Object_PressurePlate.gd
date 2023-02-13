2a7972a1052ee8c6a06be5899a3944034395b9a1
Hvnt0r <oskar.jaeger4321@web.de>
2023-02-13 13:25:05 +0100

    pressure plate down anim working
    - added animation lowering pressure plate
    - anim ofpressure plate moving up to be added

---
+++ Robot/Scenes/Object_PressurePlate.gd
@@ -0,0 +1,18 @@
+
+extends Spatial
+
+onready var anim_player = $AnimationPlayer
+
+var player = false
+
+#func _ready():
+#	anim_player.play("move_block")
+
+func do_animation():
+	player = true
+
+func _physics_process(delta):
+	if player:
+		if !anim_player.is_playing():
+			player = false
+		anim_player.play("PressurePlate_Animation")
