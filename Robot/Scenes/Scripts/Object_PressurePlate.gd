1c296e01db4f7d5424a557dda94e9845783d8b44
Hvnt0r <oskar.jaeger4321@web.de>
2023-02-20 13:31:12 +0100

    NIX FUNKTIONER
    scheiß godot

--- Robot/Scenes/Scripts/Object_PressurePlate.gd
+++ Robot/Scenes/Scripts/Object_PressurePlate.gd
@@ -4,6 +4,7 @@ extends Spatial
 onready var anim_player = $AnimationPlayer
 
 var player = false
+#var timer1 = 2
 
 #func _ready():
 #	anim_player.play("move_block")
@@ -12,7 +13,13 @@ func do_animation():
 	player = true
 
 func _physics_process(delta):
+	#timer1 = timer1 - delta
 	if player:
 		if !anim_player.is_playing():
-			player = false
-		anim_player.play("PressurePlate_Animation")
+			anim_player.play("Pressure_Plate_down")
+		player = false
+#	if player nicht in area --> hoch
+#	if timer1 <= 0:
+#		if !anim_player.is_playing():
+#			anim_player.play("Pressure_Plate_up")
+#		player = false
