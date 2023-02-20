2b7001ab00593923f6a60bee12ebfcf17a7772e8
lasiv <benni@anagami.de>
2023-02-20 13:06:48 +0100

    pause game

--- Robot/Scenes/Scripts/UI_PreGameScreen.gd
+++ Robot/Scenes/Scripts/UI_PreGameScreen.gd
@@ -14,21 +14,25 @@ var text3 = "The monsters name was leaked. JOHN has not been careful enough when
 var text4 = "This is JOHNs lair. Now he can't go back to his own world anymore so he will have to bring harm to this one. He has built himself a fortress. Not the Flying Fortress of course."
 var texts = [text1, text2, text3, text4]
 
+onready var world = get_node("../../World")
+
 func _draw():
-	pass # move cam
+	world.set_is_paused(true)
 
 func _on_HomeButton_pressed():
 	emit_signal("closePre")
 	emit_signal("closeWorld")
 	emit_signal("openHome")
+	world.set_is_paused(false)
 
 
 func _on_PlayButton_pressed():
 	emit_signal("closePre")
+	world.set_is_paused(false)
 
 
 func _on_LevelSelectButton_pressed(id):
 	$Control/PanelContainer/MarginContainer/Label.set_text(texts[id])
 	level_id = id
 	emit_signal("chooseLevel", id)
-	# do spawnpoint
+	world.set_is_paused(false)
