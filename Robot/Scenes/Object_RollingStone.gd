3ca33b9120a211a427b7880fe5da764cc0691a1d
xHi33 <122915745+xHi33@users.noreply.github.com>
2023-03-03 11:33:26 +0100

    rolling stone

---
+++ Robot/Scenes/Object_RollingStone.gd
@@ -0,0 +1,19 @@
+extends Spatial
+
+#caches path for creating instance of RollingStone.tscn
+#const ROLLINGSTONE = preload("res://Scenes/Object_RollingStone.tscn")
+#var allowShoot = true
+#var shootimer = 0
+#var bulletdelay = 1
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#var rollingStone = ROLLINGSTONE.instance()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
