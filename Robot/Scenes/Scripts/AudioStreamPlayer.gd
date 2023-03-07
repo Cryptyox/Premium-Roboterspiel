b54b42b0a24d576563618b914b3f223a20161f20
lasiv <benni@anagami.de>
2023-03-07 09:43:35 +0100

    idk

---
+++ Robot/Scenes/Scripts/AudioStreamPlayer.gd
@@ -0,0 +1,12 @@
+extends AudioStreamPlayer
+
+
+func _process(delta):
+	pass
+	#if get_node("../../Robot").timer_started:
+	#play()
+	
+	#if get_parent().visible && get_node("../../Robot").timer_started:
+	#	play()
+	#else:
+		#stop()
